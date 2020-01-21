300356f2b22f4279001f667b81b86241c348065b
Braeden Grimes <bdgrimes0@gmail.com>
2020-01-20 20:58:56 -0600

    Setting up feature folders for dashboard. Adding routing. Added NavBar.

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import NavBar from './app/NavBar';
+import App from './app/App';
+
+const routing = (
+    <Router>
+        <NavBar />
+        <Route path="/" component={App} />
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
