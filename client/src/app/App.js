6f09487a93cc718c2047c12fb0e8f07ffa328792
Braeden Grimes <bdgrimes0@gmail.com>
2020-01-20 21:18:42 -0600

    Add some text to the main app

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -5,7 +5,8 @@ const App = () => {
   return (
     <>
       <Container>
-        
+        <h2>Welcome to the Guild Management System</h2>
+        <p>An all-in-on platform designed to make it easy to track every aspect of your guild in Classic WoW. One location for attendence, loot, and performance tracking.</p>
       </Container>
     </>
   );
